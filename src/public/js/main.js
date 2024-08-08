f0c58e9cd781d81427a079c6fa42cdd53dd24033
cbmanu <carrero.besson@gmail.com>
2024-08-08 13:29:17 -0400

    Actualizacion estilos 2024

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,24 +1,38 @@
-let text = document.querySelector('.main-title');
+// let navbar = document.querySelector(".navbar");
+// var banner_height = $("#navscroll").height();
+// var lastScrollTop = 0;
+// window.scroll(function () {
+//   var scroll = $(window).scrollTop();
+//   var currScrollTop = $(this).scrollTop();
+//   if (scroll >= banner_height && currScrollTop > lastScrollTop) {
+//     navbar.style.backgroundColor = "";
+//   } else {
+//     navbar.style.backgroundColor = "blue";
+//   }
+//   lastScrollTop = currScrollTop;
+// });
+
+let text = document.querySelector(".main-title");
 let strText = text.textContent;
-let splitText = strText.split('');
-text.textContent = ''
+let splitText = strText.split("");
+text.textContent = "";
 
 for (let i = 0; i < splitText.length; i++) {
-     text.innerHTML += '<span>' + splitText[i] + '</span>'
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
 }
 let char = 0;
-let timer = setInterval(logo,50);
+let timer = setInterval(logo, 50);
 
-function logo(){
-     let span = text.querySelectorAll('span')[char];
-     span.classList.add('animation');
-     char++;
-     if(char === splitText.length){
-          complete();
-          return;
-     }
+function logo() {
+  let span = text.querySelectorAll("span")[char];
+  span.classList.add("animation");
+  char++;
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
 }
-function complete(){
-      clearInterval(timer);
-      timer = null
+function complete() {
+  clearInterval(timer);
+  timer = null;
 }
